029b682a54040cb78ff5e2a14dd0e3b0c9f4a18b
dk9394 <k.deepak9394@gmail.com>
2026-01-03 21:37:38 +0530

    fix: test lighthouse

--- src/app/app.ts
+++ src/app/app.ts
@@ -33,6 +33,7 @@ export class App implements OnInit {
 
     // Log performance summary in console
     if (typeof ngDevMode !== 'undefined' && ngDevMode) {
+      console.warn('DevMode: ', ngDevMode);
       console.warn(this.performanceService.getPerformanceSummary());
     }
   }
